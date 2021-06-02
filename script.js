a1a958d66021206c0c2fd9ac2c827f886250f82f
알렉산더 <sasha.dyu11@gmail.com>
2021-06-02 16:34:31 +0900

    animation eddited

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 const stars = document.getElementById('stars');
 const moon = document.getElementById('moon');
 const mountains_back = document.getElementById('mountains_back');
-const text = document.getElementById('text');
+const text1 = document.getElementById('text1');
+const text2 = document.getElementById('text2');
 const btn = document.getElementById('btn');
 const mountains_front = document.getElementById('mountains_front');
 
@@ -11,7 +12,9 @@ window.addEventListener('scroll', function(){
      moon.style.top = value * 1 + 'px'; 
      mountains_back.style.top = value * 0.05 + 'px'; 
      mountains_front.style.top = value * 0 + 'px'; 
-     text.style.marginRight = value * 4 + 'px'; 
-     text.style.marginTop = value * 1.2 + 'px'; 
+     text1.style.marginRight = value * 4 + 'px'; 
+     text1.style.marginTop = value * 1.2 + 'px'; 
+     text2.style.marginLeft = value * 4 + 'px'; 
+     text2.style.marginTop = value * 1.2 + 'px'; 
      btn.style.marginTop = value * 1.5 + 'px'; 
 } )
